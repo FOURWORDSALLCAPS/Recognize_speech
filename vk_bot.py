4c729af631601cc31ccb5733a900004e0ba3ffc4
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-18 14:55:51 +0300

    Echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,26 +1,26 @@
-import vk_api
+import random
+import vk_api as vk
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
 
 
-def main():
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000)
+    )
+
+
+if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_group_token = env('VK_GROUP_TOKEN')
-    vk_session = vk_api.VkApi(token=vk_group_token)
-
+    vk_session = vk.VkApi(token=vk_group_token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
-
-
-if __name__ == '__main__':
-    main()
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
