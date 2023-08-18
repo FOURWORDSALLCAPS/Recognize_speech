ea7aeaefe043a62a55a247263b7f5f106d4acf00
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-18 15:08:46 +0300

    Add DialogFlow to VK bot

--- tg_bot.py
+++ tg_bot.py
@@ -2,32 +2,7 @@ from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandle
 from telegram import Update
 
 from environs import Env
-from google.cloud import dialogflow
-
-
-def detect_intent_texts(project_id, session_id, texts, language_code):
-    """Returns the result of detect intent with texts as inputs."""
-
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-
-    for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-        query_input = {
-            "text": {
-                "text": text_input.text,
-                "language_code": text_input.language_code
-            }
-        }
-
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-        response_dialogflow = response.query_result.fulfillment_text
-
-        return response_dialogflow
+from speech_recognition import detect_intent_texts
 
 
 def main():
--- vk_bot.py
+++ vk_bot.py
@@ -3,12 +3,13 @@ import vk_api as vk
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+from speech_recognition import detect_intent_texts
 
 
-def echo(event, vk_api):
+def echo(event, vk_api, text):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=text,
         random_id=random.randint(1, 1000)
     )
 
@@ -17,10 +18,15 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_group_token = env('VK_GROUP_TOKEN')
+    tg_user_id = env('TG_USER_ID')
+    google_project_id = env('GOOGLE_PROJECT_ID')
+
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            text = detect_intent_texts(google_project_id, tg_user_id, texts=[event.text],
+                                       language_code='ru-RU')
+            echo(event, vk_api, text)
