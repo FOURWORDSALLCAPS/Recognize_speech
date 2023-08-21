a336c11ddaba3c21261ebcf8c1c52bb2e293ab23
FOURWORDSALLCAPS <vova.zaycev39@mail.ru>
2023-08-21 12:58:33 +0300

    Add general bot training

--- create_intent.py
+++ create_intent.py
@@ -1,11 +1,12 @@
 import json
+import argparse
 
+from google.cloud import dialogflow
 from environs import Env
 
 
 def create_intent(project_id, display_name, training_phrases_parts, message_texts):
     """Create an intent of the given intent type."""
-    from google.cloud import dialogflow
 
     intents_client = dialogflow.IntentsClient()
 
@@ -23,24 +24,33 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
         display_name=display_name, training_phrases=training_phrases, messages=[message]
     )
 
-    response = intents_client.create_intent(
+    intents_client.create_intent(
         request={"parent": parent, "intent": intent}
     )
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Данный код позволяет создать намерения на сайте Doalogflow',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dest_file', type=str,
+                        help='Путь к каталогу с тренировочными данными')
+    args = parser.parse_args()
     env = Env()
     env.read_env()
     google_project_id = env('GOOGLE_PROJECT_ID')
-    with open("training_phrases.json", "r", encoding='UTF-8') as my_file:
-        file_contents = my_file.read()
+    with open(args.dest_file, "r", encoding='UTF-8') as file:
+        file_contents = file.read()
 
-    create_intent(
-        project_id=google_project_id,
-        display_name='Устройство на работу',
-        training_phrases_parts=json.loads(file_contents)['Устройство на работу']['questions'],
-        message_texts=json.loads(file_contents)['Устройство на работу']['answer']
-    )
+    file_content = json.loads(file_contents)
+    keys = file_content.keys()
+
+    for key in keys:
+        create_intent(
+            project_id=google_project_id,
+            display_name=key,
+            training_phrases_parts=file_content[key]['questions'],
+            message_texts=file_content[key]['answer']
+        )
 
 
 if __name__ == '__main__':
